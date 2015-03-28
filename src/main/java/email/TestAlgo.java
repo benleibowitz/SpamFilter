910d42ce8fa8b14559da715343132418f16ec894
benleibowitz <benleibowitz2013@gmail.com>
2015-03-28 13:38:06 -0400

    Test algorithm

--- src/main/java/email/TestAlgo.java
+++ src/main/java/email/TestAlgo.java
@@ -2,11 +2,19 @@ package email;
 
 
 public class TestAlgo implements SpamAlgorithm {
+    public TestAlgo(BayesEmailScoringSystem scoringSystem) {
+        
+    }
+    
+    public TestAlgo() {
+        
+    }
+    
 	@Override
 	public boolean isSpam(Email email) {
 		
 		//I call this one, AssumptionAlgorithm :)
-		System.out.println("Running test algorithm on email from sender:" + email.getSender());
+		System.out.println("Running test algorithm on email from sender: " + email.getSender());
 		return true;
 	}
 }
