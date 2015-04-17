610e8bff201a6f958a9af3f234ed64db5a9f3fdd
benleibowitz <benleibowitz2013@gmail.com>
2015-04-16 20:11:22 -0400

    Fix import issue

--- src/main/java/email/BayesEmailScoringSystem.java
+++ src/main/java/email/BayesEmailScoringSystem.java
@@ -15,6 +15,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,8 +24,6 @@ import org.springframework.stereotype.Component;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
-
-import edu.emory.mathcs.backport.java.util.Arrays;
 
 @Component
 public class BayesEmailScoringSystem {
--- src/test/java/email/BayesEmailAlgorithmTest.java
+++ src/test/java/email/BayesEmailAlgorithmTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class BayesEmailAlgorithmTest {
-
+    
     @Test
     public void testIsSpam() {
         BayesEmailScoringSystem scoringSystem = new BayesEmailScoringSystem();
