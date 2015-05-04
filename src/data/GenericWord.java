2141b48b520616eeea46c4d4ce875715ec110bdc
benleibowitz <benleibowitz@users.noreply.github.com>
2015-05-04 17:22:07 -0400

    Update GenericWord.java

--- src/data/GenericWord.java
+++ src/data/GenericWord.java
@@ -1,15 +1,22 @@
 package data;
 
 public class GenericWord extends Word {
+    @Override
     public int getSpamCount() {
       throw new UnsupportedOperationException("Cannot get word count on generic word. Generic word has no word count");
     }
+    
+    @Override
     public void setSpamCount(int spamCount) {
       throw new UnsupportedOperationException("Cannot set word count on generic word. Generic word has no word count");
     }
+    
+    @Override
     public int getRealCount() {
       throw new UnsupportedOperationException("Cannot get word count on generic word. Generic word has no word count")
     }
+    
+    @Override
     public void setRealCount(int realCount) {
       throw new UnsupportedOperationException("Cannot set word count on generic word. Generic word has no word count")
     }
