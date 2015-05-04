c1da3b140e363f512d130ca58767632ed0d1aff0
benleibowitz <benleibowitz@users.noreply.github.com>
2015-05-04 12:38:36 -0400

    Update WordRowMapper.java

--- src/data/WordRowMapper.java
+++ src/data/WordRowMapper.java
@@ -9,11 +9,19 @@ public class WordRowMapper implements RowMapper<Word> {
 
     @Override
     public Word mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Word word = new Word();
+        Word word = null
+        ResultSetMetaData rsMetaData = rs.getMetaData();
 
-        word.setSpamCount(rs.getInt("spam_count"));
-        word.setRealCount(rs.getInt("real_count"));
-        word.setWord(rs.getString("word"));
+        if(rsMetaData.getColumnCount > 0) {
+            
+            if(rsMetaData.getTableName().equals("generic_words")) {
+                word = new GenericWord(rs.getString("word"));
+            } else {
+                word = new Word(rs.getString("word"));
+                word.setSpamCount(rs.getInt("spam_count"));
+                word.setRealCount(rs.getInt("real_count"));
+            }
+        }
 
         return word;
     }
