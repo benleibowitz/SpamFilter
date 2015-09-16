b267494eee95db2a17a816f689f0395658bd5c7e
benleibowitz <benleibowitz2013@gmail.com>
2015-09-16 18:16:40 -0400

    testing travis

--- src/resources/MOCK_DATA.sql
+++ src/resources/MOCK_DATA.sql
@@ -1,18 +1,18 @@
-INSERT INTO generic_words VALUES ('the');
-INSERT INTO generic_words VALUES ('and');
-INSERT INTO generic_words VALUES ('an');
-INSERT INTO generic_words VALUES ('is');
-INSERT INTO generic_words VALUES ('of');
-INSERT INTO generic_words VALUES ('in');
+INSERT INTO generic_words (word) VALUES ('the');
+INSERT INTO generic_words (word) VALUES ('and');
+INSERT INTO generic_words (word) VALUES ('an');
+INSERT INTO generic_words (word) VALUES ('is');
+INSERT INTO generic_words (word) VALUES ('of');
+INSERT INTO generic_words (word) VALUES ('in');
 
 INSERT INTO body (word, spam_count, real_count) VALUES ('foo', 30, 2);
-INSERT INTO sender (word, spam_count, real_count) VALUES ('sex', 10, 1);
-INSERT INTO subject (word, spam_count, real_count) VALUES ('sex', 10, 1);
 INSERT INTO body (word, spam_count, real_count) VALUES ('sex', 10, 1);
 INSERT INTO body (word, spam_count, real_count) VALUES ('hey', 8, 5);
-INSERT INTO sender (word, spam_count, real_count) VALUES ('buddy', 10, 1);
-INSERT INTO subject (word, spam_count, real_count) VALUES ('buddy', 10, 5);
 INSERT INTO body (word, spam_count, real_count) VALUES ('buddy', 10, 8);
+INSERT INTO sender (word, spam_count, real_count) VALUES ('sex', 10, 1);
+INSERT INTO sender (word, spam_count, real_count) VALUES ('buddy', 10, 1);
+INSERT INTO subject (word, spam_count, real_count) VALUES ('sex', 10, 1);
+INSERT INTO subject (word, spam_count, real_count) VALUES ('buddy', 10, 5);
 INSERT INTO subject (word, spam_count, real_count) VALUES ('nadine', 1, 0);
 INSERT INTO subject (word, spam_count, real_count) VALUES ('left', 1, 0);
 INSERT INTO subject (word, spam_count, real_count) VALUES ('couple', 1, 0);
