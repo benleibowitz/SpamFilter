67cc51c54bd0d8e7bb5340a18b35bdd4555ba50a
benleibowitz <benleibowitz2013@gmail.com>
2015-09-26 22:26:46 -0400

    Rename mock_data.sql, add unit test

---
+++ src/test/java/test/TestEmail.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import email.Email;
+import email.ProbabilityCalculator;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles="dev")
+@ContextConfiguration(locations={"classpath:emailbeans.xml"})
+public class TestEmail {
+    @Autowired
+    private ProbabilityCalculator probCalc;
+    
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testNotNull() {
+        assertNotNull(probCalc);
+    }
+    
+    @Test
+    public void testProbCalc() {
+        Email e = new Email("Frank McJohnson", "Here is the job opportunity you requested",
+                "untz untz untz");
+        assertTrue(probCalc.isSpam(e));
+    }
+
+}
